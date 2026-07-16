a59b77eeab7eedc7ab0ebf7cc5b4f8bd479b6b2b
nupreti22 <nupreti@american.edu>
2026-07-16 20:02:27 +0530

    Added changes

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 // It must be requested via a P2 support ticket from your AEM CS program admin.
 // See: https://github.com/adobe/aem-assets-selectors-mfe-examples
 const CONFIG = {
-  IMS_CLIENT_ID: 'aemcs-tadigital-americas-assetselector',
+  IMS_CLIENT_ID: 'aemcs-techaspectamericasbx-assetselector',
   IMS_ORG: '856F5BDE58C158A50A495D50@AdobeOrg', // e.g. "ABC123@AdobeOrg", or null to let the user pick
   IMS_SCOPE: 'AdobeID,openid,additional_info.projectedProductContext,read_organizations',
   REDIRECT_URL: window.location.href,
